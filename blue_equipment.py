7adb60f28ada716524813013629ea75ac258d2be
Marc Geraerts <marc.geraerts@uhasselt.be>
2021-02-25 16:01:05 +0100

    Deans first changes

--- blue_equipment.py
+++ blue_equipment.py
@@ -20,3 +20,26 @@ def get_equipment():
     data = c.fetchall()
     return json.dumps(data)
 
+@equipment.route('/UpdateEquipment', methods=['GET'])
+def update_equipment():
+    ID = request.args.get('ID')
+    equipment_inventory_number = request.args.get('equipment_inventory_number')
+    equipment_label = request.args.get('equipment_label')
+    equipment_name = request.args.get('equipment_name')
+    equipment_amount = request.args.get('equipment_amount')
+    equipment_description = request.args.get('equipment_description')
+    equipment_outcome = request.args.get('equipment_outcome')
+    equipment_purchase_date = request.args.get('equipment_purchase_date')
+    equipment_base_location = request.args.get('equipment_base_location')
+    equipment_owner_id = request.args.get('equipment_owner_id')
+    equipment_co_owner_id = request.args.get('equipment_co_owner_id')
+    equipment_purchase_price = request.args.get('equipment_purchase_price')
+    equipment_annual_cost = request.args.get('equipment_annual_cost')
+    equipment_annual_cost_budget = request.args.get('equipment_annual_cost_budget')
+    db = Database()
+    sql_string = 'UPDATE equipment SET equipment_inventory_number="' + equipment_inventory_number + '", equipment_label="' + equipment_label + '" , equipment_name="' + equipment_name + '" , equipment_amount="' + equipment_amount + '" , equipment_description="' + equipment_description + '" , equipment_outcome="' + equipment_outcome + '" , equipment_purchase_date="' + equipment_purchase_date + '" , equipment_base_location="' + equipment_base_location + '" , equipment_owner_id="' + equipment_owner_id + '" , equipment_co_owner_id="' + equipment_co_owner_id + '" , equipment_purchase_price="' + equipment_purchase_price + '" , equipment_annual_cost="' + equipment_annual_cost + '" , equipment_annual_cost_budget="' + equipment_annual_cost_budget + '" WHERE ID = ' + ID
+    print(sql_string)
+    c = db.conn.cursor()
+    c.execute(sql_string)
+    db.conn.commit()
+    return 'http200'
--- static/js/equipment.js
+++ static/js/equipment.js
@@ -8,7 +8,6 @@ $(document).ready(function () {
 
 
     GetEquipment();
-    
 
 
 
@@ -28,8 +27,9 @@ function GetEquipment() {
             tableHTML = tableHTML.concat("</table>");
 
             $("#equipmentTable").html(tableHTML);
-            addRowHandlers();
+            // addRowHandlers();
 
+            SelectRow(g_SelectedRow);
         }
     });
 }
@@ -47,6 +47,10 @@ function GenerateTableHeader() {
 
     out = out.concat('<th>');
     out = out.concat('Outcome');
+    out = out.concat("</th>");
+
+    out = out.concat('<th>');
+    out = out.concat('Purchase Price');
     out = out.concat("</th>");
 
     out = out.concat("</tr>");
@@ -71,6 +75,10 @@ function GenerateTabelRow(rownNr, equipment) {
     out = out.concat(getTableDiv(equipment.equipment_outcome, 200, rownNr));
     out = out.concat("</td>");
 
+    out = out.concat('<td>');
+    out = out.concat(getTableDiv(equipment.equipment_purchase_price, 75, rownNr));
+    out = out.concat("</td>");
+
     out = out + "</tr>";
 
     return out;
@@ -90,23 +98,71 @@ function getTableDiv(content, width, rownNr) {
     return out;
 }
 
-function SelectRow(rownNr){
-
+function SelectRow(rownNr) {
     var table = document.getElementById("equipmentTable");
     var rows = table.getElementsByTagName("tr");
-    if (g_SelectedRow>-1){
+    if (g_SelectedRow > -1) {
         PreviouslyselectedRow = rows[g_SelectedRow];
         selectedRow.style.backgroundColor = null;
     }
     selectedRow = rows[rownNr];
-    selectedRow.style.backgroundColor  = '#A7DF62';
+    selectedRow.style.backgroundColor = '#A7DF62';
     g_SelectedRow = rownNr;
-    g_selectedEquipment = g_equipment[rownNr-1];
+    g_selectedEquipment = g_equipment[rownNr - 1];
+    $("#eq_inventory_number").val(g_selectedEquipment.equipment_inventory_number);
+    $("#eq_label").val(g_selectedEquipment.equipment_label);
     $("#eq_name").val(g_selectedEquipment.equipment_name);
+    $("#eq_amount").val(g_selectedEquipment.equipment_amount);
     $("#eq_desc").val(g_selectedEquipment.equipment_description);
     $("#eq_outcome").val(g_selectedEquipment.equipment_outcome);
+    $("#eq_purchase_date").val(g_selectedEquipment.equipment_purchase_date);
+    $("#eq_base_location").val(g_selectedEquipment.equipment_base_location);
+    if (g_selectedEquipment.equipment_is_mobile == 1) {
+        $("#eq_is_mobile").prop("checked", true);
+    } else if (g_selectedEquipment.equipment_is_mobile == -1) {
+        $("#eq_is_mobile").prop("checked", false);
+    } else {
+        $("#eq_is_mobile").prop("checked", false);
+    }
+
+    $("#eq_owner_id").val(g_selectedEquipment.equipment_owner_id);
+    $("#eq_co_owner_id").val(g_selectedEquipment.equipment_co_owner_id);
+    $("#eq_purchase_price").val(g_selectedEquipment.equipment_purchase_price);
+    $("#eq_annual_cost").val(g_selectedEquipment.equipment_annual_cost);
+    $("#eq_annual_cost_budget").val(g_selectedEquipment.equipment_annual_cost_budget);
 
 }
+
+function UpdateEquip() {
+    // g_selectedEquipment = g_equipment[rownNr - 1];
+    var ID = g_selectedEquipment.ID;
+    var equipment_inventory_number = $("#eq_inventory_number").val();
+    var equipment_label = $("#eq_label").val();
+    var equipment_name = $("#eq_name").val();
+    var equipment_amount = $("#eq_amount").val();
+    var equipment_description = $("#eq_desc").val();
+    var equipment_outcome = $("#eq_outcome").val();
+    var equipment_purchase_date = $("#eq_purchase_date").val();
+    var equipment_base_location = $("#eq_base_location").val();
+    var equipment_owner_id = $("#eq_owner_id").val();
+    var equipment_co_owner_id = $("#eq_co_owner_id").val();
+    var equipment_purchase_price = $("#eq_purchase_price").val();
+    var equipment_annual_cost = $("#eq_annual_cost").val();
+    var equipment_annual_cost_budget = $("#eq_annual_cost_budget").val();
+    // var scrollPos = $("#equipmentTable").scrollTop();
+
+    if (g_SelectedRow > -1) {
+        argString = "?ID=" + ID + "&equipment_inventory_number=" + equipment_inventory_number + "&equipment_label=" + equipment_label + "&equipment_name=" + equipment_name + "&equipment_amount=" + equipment_amount + "&equipment_description=" + equipment_description + "&equipment_outcome=" + equipment_outcome + "&equipment_purchase_date=" + equipment_purchase_date + "&equipment_base_location=" + equipment_base_location + "&equipment_owner_id=" + equipment_owner_id + "&equipment_co_owner_id=" + equipment_co_owner_id + "&equipment_purchase_price=" + equipment_purchase_price + "&equipment_annual_cost=" + equipment_annual_cost + "&equipment_annual_cost_budget=" + equipment_annual_cost_budget;
+
+        console.log(argString)
+        $.get("/UpdateEquipment" + argString, function (data, status) {
+            if (data.localeCompare("http200") == 0) {
+                GetEquipment();
+            }
+        });
+    }
+}
+
 
 
 
